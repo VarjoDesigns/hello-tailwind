f66baadeb1888a246985df5417aca06384d30a09
VarjoDesigns <57039872+VarjoDesigns@users.noreply.github.com>
2021-10-16 17:34:51 +0300

    Mock up form improved

--- src/component/Form.js
+++ src/component/Form.js
@@ -2,30 +2,68 @@ import React, {useState} from 'react';
 
 export default function Form() {
 
+    const [form, setValues] = useState({
+        name: '',
+        email: '',
+        message: '',
+    });
+
+    const [message, setMessage] = useState('');
+
+    const change = (e) => {
+
+        setValues({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+        setMessage('');
+    }
+
+    const addForm = (e) => {
+        e.preventDefault();
+        
+        if(!form.name ||!form.email ||!form.message) {
+            console.log('Empty')
+            setMessage('You need to fill out all sections!')
+        } else {
+            console.log('Name: ' +form.name);
+            console.log('Form: ' +form.email);
+            console.log('Message: ' +form.message);
+
+            setValues({
+                name: '',
+                email: '',
+                message: '',
+            })
+
+            setMessage('Form was submitted! Please note that this is a demo enviroment!');
+        }
+    }
+
     return(
-        <div className="py-20">
+        <div className="mt-20">
             <h1 className="font-bold tracking-tight text-2xl text-center text-indigo-900">
                 Please leave us a message!
             </h1>
-            <form className="mx-auto text-center mt-8 text-indigo-900">
+            <form className="mx-auto text-center mt-6 text-indigo-900">
                 <div className="py-2">
                     <label htmlFor="name">Your name: </label><br />
-                    <input id="name" type="text"  className="py-2 px-4 w-80"></input>
+                    <input name="name" type="text" value={ form.name } onChange={ (e) => change(e)} className="py-2 px-4 w-80 rounded-md shadow-sm hover:shadow-lg"></input>
                 </div>
 
                 <div className="py-2">
                     <label htmlFor="name">Your email: </label><br />
-                    <input id="email" type="text" className="py-2 px-4 w-80"></input>
+                    <input name="email" type="text" value={ form.email } onChange={ (e) => change(e)} className="py-2 px-4 w-80 rounded-md shadow-sm hover:shadow-lg"></input>
                 </div>
                 
                 <div className="py-2">
                     <label htmlFor="name">Your message: </label><br />
-                    <textarea id="email" type="text" className="py-2 px-4 w-80"></textarea>
+                    <textarea name="message" type="text" value={ form.message } onChange={ (e) => change(e)} className="py-2 px-4 w-80 rounded-md shadow-sm hover:shadow-lg"></textarea>
                 </div>
 
-                <button type="submit" className="bg-indigo-900 hover:bg-indigo-800 text-white py-3 px-16 rounded-full font-light">Submit</button>
-
+                <button type="submit" onClick={ (e) => addForm(e)} className="mt-4 bg-indigo-900 hover:bg-indigo-800 text-white py-3 px-16 rounded-full font-light">Submit</button>
             </form>
+            <p className="mt-8 text-center">{message}</p>
         </div>
     )
 }
--- src/component/Learnmore.js
+++ src/component/Learnmore.js
@@ -6,7 +6,7 @@ export default function Learnmore() {
     return(
         <div className="content-center px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-24">
             <div className="relative overflow-hidden container mx-auto content-center justify-center">
-                <h1 className="font-bold tracking-tighter text-2xl text-center text-indigo-900">Lorem ipsum</h1>
+                <h1 className="font-bold tracking-tighter text-2xl text-center text-indigo-900">About the technology</h1>
                 <p className="py-2 max-w-4xl text-center mx-auto text-indigo-900">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
                 labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
@@ -19,7 +19,7 @@ export default function Learnmore() {
                 cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                 </p>
                 <div className="flex justify-center py-2">
-                    <button className="bg-pink-600 hover:bg-pink-500 text-white py-2 px-4 rounded-full">
+                    <button className="mt-4 bg-indigo-900 hover:bg-indigo-800 text-white py-3 px-16 rounded-full font-light">
                         Find out more
                     </button>
                 </div>
